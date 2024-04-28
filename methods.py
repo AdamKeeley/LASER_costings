132f146a0f6f90e3d9fbb36646465dff2b651a68
TenSeconder <email@email.com>
2024-04-28 23:59:35 +0100

    Universalised methods

--- methods.py
+++ methods.py
@@ -3,22 +3,77 @@ import requests
 import pandas as pd
 
 def getPrices(queryString):
-    api_url = f"https://prices.azure.com/api/retail/prices?currencyCode='GBP'&api-version=2021-10-01-preview"
-    response = requests.get(api_url, params={'$filter': queryString})
-    print(response.status_code)
-    json_data = json.loads(response.text)
-    nextPage = json_data['NextPageLink']
-    items = json_data['Items']
-    df = pd.json_normalize(items)
-    while(nextPage):
-        response = requests.get(nextPage)
-        print(response.status_code)
-        json_data = json.loads(response.text)
-        nextPage = json_data['NextPageLink']
-        items = json_data['Items']
-        df = pd.concat([df, pd.json_normalize(items)])
-    return df[df['tierMinimumUnits']==0]
+    if queryString:
+        api_url = f"https://prices.azure.com/api/retail/prices?currencyCode='GBP'&api-version=2021-10-01-preview"
+        response = requests.get(api_url, params={'$filter': queryString})
+        if response.status_code == 200:
+            json_data = json.loads(response.text)
+            nextPage = json_data['NextPageLink']
+            items = json_data['Items']
+            df = pd.json_normalize(items)
+            while(nextPage):
+                response = requests.get(nextPage)
+                json_data = json.loads(response.text)
+                nextPage = json_data['NextPageLink']
+                items = json_data['Items']
+                df = pd.concat([df, pd.json_normalize(items)])
+            return df[df['tierMinimumUnits']==0]
+        else:
+            return f"Failure to fetch prices (status_code = {response.status_code})."
+    else: 
+        return "No queryString provided."
 
+def getQueryStringElement(resourceType, elementType):
+    with open("resources.json", "r") as f:
+        parsed_json=json.load(f)
+    
+    query_element = ""
+    query_element_list = []
+    if parsed_json['TRE'][resourceType][elementType]:
+        for item in parsed_json['TRE'][resourceType][elementType]:
+            if parsed_json['TRE'][resourceType][elementType][item]['operator'] == 'eq':
+                query_element_list.append (
+                    f"{parsed_json['TRE'][resourceType][elementType][item]['identifier']} " + 
+                    f"{parsed_json['TRE'][resourceType][elementType][item]['operator']} " + 
+                    f"'{parsed_json['TRE'][resourceType][elementType][item]['value']}'"
+                )
+            if parsed_json['TRE'][resourceType][elementType][item]['operator'] == 'contains':
+                query_element_list.append (
+                    f"{parsed_json['TRE'][resourceType][elementType][item]['operator']}" + 
+                    f"({parsed_json['TRE'][resourceType][elementType][item]['identifier']}, " + 
+                    f"'{parsed_json['TRE'][resourceType][elementType][item]['value']}')"
+                )
+        if elementType == 'Common':
+            query_element = ' and '.join(query_element_list)
+        if elementType == 'Component' or elementType == 'Required':
+            query_element = ' or '.join(query_element_list)
+    
+    return query_element
+
+def getQueryString(resourceType):
+    with open("resources.json", "r") as f:
+        parsed_json=json.load(f)
+    
+    query_region = (
+        "(armRegionName eq 'Global' " +
+        f"or armRegionName eq '{parsed_json['armRegionName']}') " 
+        )
+    query_common = getQueryStringElement(resourceType, 'Common')
+    query_component = getQueryStringElement(resourceType, 'Component')
+    query_required = getQueryStringElement(resourceType, 'Required')
+    
+    if query_common and query_component and query_required:
+        query_full = f"{query_region} and ((({query_common}) and ({query_component})) or {query_required})"
+    elif query_common and query_component:
+        query_full = f"{query_region} and (({query_common}) and ({query_component}))"
+    elif query_component and query_required:
+        query_full = f"{query_region} and (({query_component}) or ({query_required}))"
+    elif query_component:
+        query_full = f"{query_region} and ({query_component})"
+    else:
+        query_full = ""
+    return query_full
+    
 # TODO: align VM logic with that of Storage iteration
 def vmQueryString(vm):
     with open("resources.json", "r") as f:
